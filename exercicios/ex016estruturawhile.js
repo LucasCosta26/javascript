01a1f5ccbcbb19e676fe0e4e47a03996b1944004
Lucas_oliveira186 <lucasatsoc26@gmail.com>
2022-01-05 13:40:16 -0300

    .

---
+++ exercicios/ex016estruturawhile.js
@@ -0,0 +1,16 @@
+
+var i = 1
+while ( i <= 3){// essa é uma estrutura simple do while
+    console.log(`Olá, Lucas ${i}`)
+    i++ // Se você esquecer que aumentar a variável o loop não terá fim e irá travar o terminal.
+}
+
+/*console.log("Olá, Lucas!")
+console.log("Olá, Lucas!") esse é um modo incorreto de execução
+console.log("Olá, Lucas!")*/
+
+do {
+    console.log('Lucas!');
+    i++;
+}
+while ( i <= 0)//mesmo minha condição sendo false, o (do) irá executar ao menos um vez 
